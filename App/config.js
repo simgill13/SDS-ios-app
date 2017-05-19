83c56fe79ea4edb7f3a1273426823a86db8c4153
Sim Gill <simgill89@gmail.com>
2017-05-18 17:12:57 -0700

    adding git ignore file to app folder

--- App/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-	apiKey: "AIzaAoCX6g",
-    authDomain: "sds-ios-e6c2d.firebaseapp.com",
-    databaseURL: "https://sds-ios-e6c2d.firebaseio.com",
-    projectId: "sds-ios-e6c2d",
-    storageBucket: "sds-ios-e6c2d.appspot.com",
-    messagingSenderId: "169252357358"
-}
