6749e43b372d152d0de1739194d74c599799cdae
James Guthrie <jamesrguthrie@gmail.com>
2017-05-22 17:17:19 -0400

    added search Component, and ability to add users found in search to your friends List as well as adding yourself to their list

--- App/components/search.js
+++ App/components/search.js
@@ -6,11 +6,12 @@ import {
   Text,
   StyleSheet,
   TouchableHighlight,
-  Navigator
+  Navigator,
+  TextInput
  } from 'react-native';
 import { List, ListItem } from 'react-native-elements'
 import { Components } from 'expo';
-
+import {searchUsers, addFriend} from '../actions/action';
 import ChatRoom from './chatroom';
 
 
@@ -22,20 +23,31 @@ class SearchUsers extends Component{
     }
   }
 
-
-	// chatroom(){
-	// 	console.log(this.props.navigator)
-	// 	this.props.navigator.push({
-	// 		id:"chatroom",
-	// 	})
-	// }
-
   onPressSearch(query) {
-    console.log(query);
+    this.props.dispatch(searchUsers(query))
+  }
+
+  onPressList(user){
+    // console.log(user);
+    // console.log(this.props.userId)
+    this.props.dispatch(addFriend(this.props.userId, user._id));
   }
 
   renderSearchList() {
-    console.log('hello');
+    return (
+      <List containerStyle={{marginBottom: 20}}>
+        {
+          this.props.searchedUsers.map((user, i) => (
+            <ListItem
+              key={i}
+              title={user.name}
+              onPress={() => {this.onPressList(user)}}
+              hideChevron={false}
+            />
+          ))
+        }
+      </List>
+    );
   }
 
   render(){
@@ -52,6 +64,11 @@ class SearchUsers extends Component{
             style={styles.textInput}>
           </TextInput>
         </View>
+        <View>
+          <Text
+            onPress={() => this.onPressSearch(this.state.query)}
+            >Search</Text>
+        </View>
 
         <View>
           {this.renderSearchList()}
@@ -62,9 +79,44 @@ class SearchUsers extends Component{
   }
 }
 
+const styles = {
+  inputWrap:{
+    flexDirection: 'row',
+    marginVertical: 10,
+    height:60,
+    backgroundColor: 'transparent',
+    paddingHorizontal: 10
+  },
+  textInput: {
+    flex:1,
+    alignItems: 'center',
+    textAlign: 'left',
+    color: '#222222',
+    padding: 10,
+    margin: 5,
+    borderColor: "rgba(255,255,255,.8)",
+    borderWidth: 1,
+    borderRadius: 3,
+  },
+  button: {
+    backgroundColor: "rgba(255,255,255,.3)",
+    flex:1,
+    borderColor: '#ffffff',
+    margin: 5,
+    borderRadius: 12,
+    borderColor: "rgba(255,255,255,.8)",
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    height: 50,
+    width: 50,
+  }
+}
+
 
 const mapStateToProps = (state) => ({
   userId: state.userId,
+  searchedUsers: state.searchedUsers
 });
 
 export default connect(mapStateToProps)(SearchUsers);
