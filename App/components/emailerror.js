cd67e59a18a33c94bcab80d240e25b33de57918d
Terry Hayes <terrychayes@gmail.com>
2017-05-23 11:55:12 -0400

    clean up EmailError component

--- App/components/emailerror.js
+++ App/components/emailerror.js
@@ -1,39 +1,25 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {AppRegistry,Text,View,TouchableHighlight,StyleSheet,Image,Button} from 'react-native';
-
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { AppRegistry, Text, View, TouchableHighlight, StyleSheet, Image, Button } from 'react-native';
 
 class EmailError extends Component{
 	constructor(props) {
     super(props);
   }
 
- 
-
-  render(){
-	    return(
-      		<View>
-      			<Text style={styles.test}> Sorry Someone has this email</Text>  
-      		</View>
-	    )
-    }
+	render(){
+		return(
+  		<View>
+  			<Text style={styles.test}>An account with this email already exist.</Text>
+  		</View>
+		)
+	}
 }
-
-
-
-
 
 const styles = StyleSheet.create({
   test:{
     marginLeft:100
   }
-
 });
 
-
-const mapStateToProps = (state) => ({
-
-});
-
-export default connect(mapStateToProps)(EmailError);
+export default connect()(EmailError);
