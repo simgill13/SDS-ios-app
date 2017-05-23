c69430aba4ae870f546b008b4b23e7ac90120f03
Terry Hayes <terrychayes@gmail.com>
2017-05-23 17:25:57 -0400

    Major code cleanup/consistency updates; includes name changes to fit with previous changes made to action.js

--- App/reducers/reducer.js
+++ App/reducers/reducer.js
@@ -1,63 +1,60 @@
 import {
- USER_DATA,
- EMAIL_IN_DB_TOGGLE,
- INCORRECT_EMAIL_PASSWORD,
- NEW_USER_CREATED,
- USER_LOGIN,
- UPDATE_ROOMS,
- CHANGE_LOGIN_BTN_STATE,
- CHANGE_LOGIN_BTN_STATE2,
- SEARCHED_USERS,
- ADDED_FRIEND,
- SPINNER_ON,
- SPINNER_OFF
+  USER_DATA,
+  EMAIL_IN_DB_ON,
+  EMAIL_IN_DB_OFF,
+  INCORRECT_EMAIL_PASSWORD,
+  NEW_USER_CREATED,
+  USER_LOGIN,
+  UPDATE_ROOMS,
+  LOGIN_ERROR_TRUE,
+  LOGIN_ERROR_FALSE,
+  SEARCHED_USERS,
+  ADDED_FRIEND,
+  SPINNER_ON,
+  SPINNER_OFF,
 } from '../actions/action';
 
 const initialState = {
-    name: '',
-    email:'',
-    friendsList: [],
-    userId: '',
-    deviceToken: '',
-    rooms: [],
-    emailInDb:false,
-    newUserCreated:false,
-    incorrectEmailOrPassword: false,
-    userLogin: false,
-    LoginButtonError:false,
-    activityIndicator:false,
-    searchedUsers: [],
-    spinner:false
+  name: '',
+  email:'',
+  friendsList: [],
+  userId: '',
+  deviceToken: '',
+  rooms: [],
+  emailInDb: false,
+  newUserCreated: false,
+  incorrectEmailOrPassword: false,
+  userLogin: false,
+  LoginButtonError: false,
+  activityIndicator: false,
+  searchedUsers: [],
+  spinner: false,
 }
 
 export default (state = initialState, action) => {
 	switch(action.type) {
 		case USER_DATA:
-      console.log('userObj', action.userObj)
-			return Object.assign({}, state, {
+      return {...state,
 				name: action.userObj.name,
 				email: action.userObj.email,
         friendsList: action.userObj.friendsList,
         userId: action.userObj.id,
         deviceToken: action.userObj.deviceToken,
-        rooms: action.userObj.rooms
-			});
+        rooms: action.userObj.rooms,
+			};
     case SEARCHED_USERS:
       return {...state, searchedUsers: action.data}
     case ADDED_FRIEND:
       return {...state, friendsList: action.data.friendsList}
-		case EMAIL_IN_DB_TOGGLE:
-			return Object.assign({}, state, {
-				emailInDb: !state.emailInDb
-			});
+		case EMAIL_IN_DB_ON:
+			return {...state, emailInDb: true}
+    case EMAIL_IN_DB_OFF:
+      return {...state, emailInDb: false}
     case INCORRECT_EMAIL_PASSWORD:
       return {...state, incorrectEmailOrPassword: true }
 		case NEW_USER_CREATED:
-			return Object.assign({}, state, {
-				newUserCreated: true
-			});
+      return {...state, newUserCreated: true }
     case USER_LOGIN:
-      console.log('userObj', action.userObj);
       return {...state,
         userLogin: true,
         name: action.name,
@@ -68,28 +65,16 @@ export default (state = initialState, action) => {
         rooms: action.userObj.rooms
       }
     case UPDATE_ROOMS:
-      console.log(action.data);
-      return {
-        ...state,
-        rooms: [...state.rooms, action.data]
-      }
-      case CHANGE_LOGIN_BTN_STATE:
-      return Object.assign({}, state, {
-        LoginButtonError: true
-      });
-      case CHANGE_LOGIN_BTN_STATE2:
-      return Object.assign({}, state, {
-        LoginButtonError: false
-      });
-      case SPINNER_ON:
-      return Object.assign({}, state, {
-        spinner: true
-      });
-      case SPINNER_OFF:
-      return Object.assign({}, state, {
-        spinner: false
-      });
-	    default:
-	        return state;
+      return {...state, rooms: [...state.rooms, action.data]}
+    case LOGIN_ERROR_TRUE:
+      return {...state, LoginButtonError: true}
+    case LOGIN_ERROR_FALSE:
+      return {...state, LoginButtonError: false}
+    case SPINNER_ON:
+      return {...state, spinner: true}
+    case SPINNER_OFF:
+      return {...state, spinner: false}
+    default:
+      return state;
 	}
 }
